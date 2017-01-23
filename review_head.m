d909467b56a9bb44f61847c04f2273b03dc103a7
alex <alexandershaw4@gmail.com>
2017-01-23 12:41:56 +0000

    updated

--- review_head.m
+++ review_head.m
@@ -1,4 +1,7 @@
 function review_head(S)
 
-scatter3(S.REF(:,1),S.REF(:,2),S.REF(:,3)); hold on;
+%scatter3(S.REF(:,1),S.REF(:,2),S.REF(:,3)); hold on;
+scatter3(S.EEG(:,1),S.EEG(:,2),S.EEG(:,3),'b','filled'); hold on
+scatter3(S.MEG(:,1),S.MEG(:,2),S.MEG(:,3),'r','filled'); 
+
 set(gca,'visible','off');
